ce679c9920a8b6ad971ec4878ec850c1a9354cf0
Seth Hinz <sethhinz@me.com>
2021-03-04 13:35:09 -0800

    Adding lots of interactivity, data binding

--- ui.js
+++ ui.js
@@ -7,20 +7,45 @@ let errorMessage = document.getElementById("error")
 
 let scriptPreview = document.getElementById("script-preview").content
 
+function onEnableChanged(script, enabled) {
+	script.enabled = enabled
+
+	saveScript(script).catch(() => {
+		errorMessage.innerText = "Could not save changes!"
+	})
+
+	// TODO: send a message to the background script to actually disable / enable the userscript
+}
+
+function onEditPressed(script) {
+	homepage.classList.remove("active")
+	editor.classList.add("active")
+}
+
+function onLeaveEditorPressed() {
+	editor.classList.remove("active")
+	homepage.classList.add("active")
+}
+
+editor.querySelector("button#leave-editor").addEventListener("click", onLeaveEditorPressed)
+
 getAllScripts().then(scripts => {
 	Object.keys(scripts).forEach(uuid => {
 		let script = scripts[uuid]
 		let listItem = scriptPreview.cloneNode(true).children[0]
 
-		listItem.querySelector("h3").innerText = script.name
-
 		let h4s = listItem.querySelectorAll("h4")
+		let checkbox = listItem.querySelector("input[type=checkbox]")
+		let editButton = listItem.querySelector("button")
+
+		listItem.querySelector("h3").innerText = script.name
 		h4s[0].innerText = script.matches.toString()
 		h4s[1].innerText = script.date.toLocaleDateString()
-
-		listItem.querySelector("input[type=checkbox]").checked = script.enabled
-
+		checkbox.checked = script.enabled
 		listItem.dataset.uuid = uuid
+
+		checkbox.addEventListener("click", event => onEnableChanged(script, event.target.checked))
+		editButton.addEventListener("click", () => onEditPressed(script))
 
 		scriptList.appendChild(listItem)
 	})
